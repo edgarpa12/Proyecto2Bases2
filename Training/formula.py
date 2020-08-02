b6f21e5ae90ee31b18111b4f34f41bfe5fddd700
Edgar <edgdav00@gmail.com>
2020-08-02 01:32:04 -0600

    Formula de coeficiente

---
+++ Training/formula.py
@@ -0,0 +1,5 @@
+class Formula:
+    def __init__(self, pRuta, pCoeficientesA, pCoeficienteB):
+        self.ruta = pRuta
+        self.coeficientesA = pCoeficientesA
+        self.coeficienteB = pCoeficienteB

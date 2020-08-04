cc34f38cacdd8a5dc1d01b4394a16aed87b7d76f
Kendall Tames <tamesfern@gmail.com>
2020-08-04 13:37:47 -0600

    Open Table

--- MapR/src/bigtest/ElMaper.java
+++ MapR/src/bigtest/ElMaper.java
@@ -1,28 +1,26 @@
 package bigtest;
 
 import java.io.IOException;
-import java.text.ParseException;
 
 import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hive.hcatalog.data.HCatRecord;
 import org.apache.hive.hcatalog.data.schema.HCatSchema;
 import org.apache.hive.hcatalog.mapreduce.HCatBaseInputFormat;
 
-public class ElMaper extends Mapper<LongWritable, HCatRecord, Text, Text> {
+public class ElMaper extends Mapper<WritableComparable, HCatRecord, Text, Text> {
 
 	
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void map(WritableComparable key, HCatRecord value, org.apache.hadoop.mapreduce.Mapper.Context context)
-			throws IOException, InterruptedException, ParseException {
+			throws IOException, InterruptedException {
 
 		// Get table schema
 		HCatSchema schema = HCatBaseInputFormat.getTableSchema(context.getConfiguration());
 
-		// transaccionid, fechacompra, userid, monto, origen, destino
+		// fechacompra, userid, monto, origen, destino, mes, ano
 
 		String fechacomprap = new String(value.getString("fechacompra", schema));
 		String destinop = value.getString("destino", schema);
--- MapR/src/bigtest/StartJobMain.java
+++ MapR/src/bigtest/StartJobMain.java
@@ -1,11 +1,12 @@
 package bigtest;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hive.hcatalog.mapreduce.HCatInputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.FloatWritable;
@@ -13,18 +14,26 @@ import org.apache.hadoop.io.FloatWritable;
 public class StartJobMain extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
+		System.out.println("Ola Edgar");
 		int res = ToolRunner.run(new StartJobMain(), args);
 		System.exit(res);
 	}
 
 	public int run(String[] args) throws Exception {
-		Job job = new Job(getConf(), "sumar ventas por anno");
+		Configuration conf = getConf();
+        conf.set("mapred.compress.map.output", "true");
+        conf.set("mapred.output.compression.type", "BLOCK");
+        
+		Job job = new Job(conf, "Sumar cantidad de visitas por parte del mes");
 		job.setJarByClass(this.getClass());
 
 		System.out.println(args[0]);
 		// Use TextInputFormat, the default unless job.setInputFormatClass is used
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path("/user/cloudera/yearsales/"));
+		HCatInputFormat.setInput(job, args[0], args[1]);
+
+		FileOutputFormat.setOutputPath(job, new Path("/user/cloudera/proyectobases/"));
+		
+		job.setInputFormatClass(HCatInputFormat.class);
 		job.setJarByClass(ElMaper.class);
 		job.setReducerClass(ElReducer.class);
 		job.setOutputKeyClass(IntWritable.class);

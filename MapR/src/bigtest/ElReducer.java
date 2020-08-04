72361cafd8da57a8e3e0fcac377603fd2619ee45
Kendall Tames <tamesfern@gmail.com>
2020-08-03 23:40:15 -0600

    1.7

--- MapR/src/bigtest/ElReducer.java
+++ MapR/src/bigtest/ElReducer.java
@@ -1,9 +1,10 @@
 package bigtest;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.io.IOException;
-import java.time.YearMonth;
+//import java.time.YearMonth;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -16,22 +17,22 @@ public class ElReducer extends Reducer<Text, Text, KeyType, IntWritable> {
 	@Override
 	public void reduce(Text destino, Iterable<Text> salidas, Context context) throws IOException, InterruptedException {
 
-	
 		HashMap<String, Integer> data = new HashMap<String, Integer>();
 
 		// yyyy-mm-parte -> cantidad de visitas
 
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		YearMonth ym;
+//		YearMonth ym;
 		double daysCount;
 
 		for (Text d : salidas) {
 			try {
 				cal.setTime(format.parse(d.toString()));
 
-				ym = YearMonth.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1);
-				daysCount = ym.lengthOfMonth();
+//				ym = YearMonth.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1);
+				daysCount = (new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+						cal.get(Calendar.DAY_OF_MONTH))).getActualMaximum(Calendar.DAY_OF_MONTH);
 
 				double daysPerPart = (daysCount + 1) / 4;
 
@@ -51,16 +52,16 @@ public class ElReducer extends Reducer<Text, Text, KeyType, IntWritable> {
 		}
 
 		for (String key : data.keySet()) {
-			String[] dateParts =  key.split("-");
+			String[] dateParts = key.split("-");
 
 			IntWritable out = new IntWritable(data.get(key));
 			KeyType kt = new KeyType();
-			
+
 			kt.destino = destino.toString();
 			kt.year = Integer.parseInt(dateParts[0]);
 			kt.month = Integer.parseInt(dateParts[1]);
 			kt.monthPart = Integer.parseInt(dateParts[2]);
-			
+
 			context.write(kt, out);
 		}
 	}

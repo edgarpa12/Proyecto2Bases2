a339f597b32c2419406148b3b538c231c3210c6d
Steven Quesada <stevenq161198@gmail.com>
2020-08-04 23:11:36 -0600

    gitflow-feature-stash: API

---
+++ ApiConnection/index.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'svc-4d649326-d551-4380-857b-3b28db85d594-dml.aws-virginia-1.db.memsql.com',
+    // host: 'localhost',
+    user: 'admin',
+    password: 'Sqj161198',
+    database: 'memsqlApp'
+});
+
+db.connect((err) => {
+    if(err){
+      console.log('Error connecting to Db');
+      return;
+    }
+    console.log('Connection established');
+});
+
+db.query('SELECT * FROM test', (err,rows) => {
+    if(err) throw err;
+  
+    console.log('Data received from Db:');
+    console.log(rows);
+});
